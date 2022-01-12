c7e54b28ea1a75c25df6b9c5d848a6c76c670205
Rachel Rafik <rrafik4358@stu.d214.org>
2022-01-12 10:58:54 -0600

    tried writing the non-firebase code

--- trello/ViewController.swift
+++ trello/ViewController.swift
@@ -7,13 +7,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var items:[String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
-
+    @IBAction func whenBarButtonPressed(_ sender: Any) {
+        let newitem = textField.text!
+        items.append(newitem)
+        tableView.reloadData()
+        textField.text = ""
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell")!
+        cell.textLabel?.text = items[indexPath.row]
+        return cell
+    }
+    
 }
 
